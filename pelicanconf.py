d16a0e2ed62c6e8bc428009ec1f88117bceb4f4d
vladinagdimaev <140087083+vladinagdimaev@users.noreply.github.com>
2026-09-14 15:02:24 +0300

    Add Pelican development configuration

---
+++ pelicanconf.py
@@ -0,0 +1,25 @@
+AUTHOR = 'Toma'
+SITENAME = 'Toma Beauty Styling'
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'Asia/Jerusalem'
+DEFAULT_LANG = 'he'
+
+THEME = 'theme'
+OUTPUT_PATH = 'output/'
+STATIC_PATHS = []
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+DEFAULT_PAGINATION = False
+DIRECT_TEMPLATES = ['index']
+
+RELATIVE_URLS = True
+DELETE_OUTPUT_DIRECTORY = True
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
